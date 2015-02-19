b586af289c2e1a5d968187888002bd6d304f051c
ssburns <ssburns@gmail.com>
2015-02-19 07:10:15 -0800

    more

--- samples/main1.go
+++ samples/main1.go
@@ -5,15 +5,58 @@ import (
 	tcm "CSCI130-Go/tmmath"
 )
 
+func newSum (intArrayParam [3]int) int {
+	total := 0
+
+	for _, x := range intArrayParam {
+		total += x
+	}
+
+	return total
+}
+
 func main() {
 
-
-	//mathy things
+	//
+	//mathy package things
+	//
 	numberSlice := []int{10,20,30}
 	sumOfNumbers := tcm.Sum(numberSlice)
 	fmt.Println(sumOfNumbers)
 	avgOfNumbers := tcm.Average(numberSlice)
 	fmt.Println(avgOfNumbers)
-	
 
+	//
+	//slices
+	//
+	mySlice := []int{1,2,3,4}
+	myOtherSlice := []int{5,6,7,8,9}
+
+	var myBiggerSlice []int
+	myBiggerSlice = append(mySlice, myOtherSlice...)
+
+	fmt.Println("myslice - ", mySlice)
+	fmt.Println("myOtherSlice - ", myOtherSlice)
+	fmt.Println("myBiggerSlice - ", myBiggerSlice)
+
+	var smallerThanBiggerSlice []int
+	smallerThanBiggerSlice = append(mySlice[2:4], myOtherSlice[1:3]...)
+	fmt.Println("smallerThanBiggerSlice - ", smallerThanBiggerSlice)
+
+	//
+	//slices vs arrays
+	//
+	numberSlice = []int{5,10,15}
+	numberArray := [3]int{5,10,15}
+	fmt.Println("slice sum = ", tcm.Sum(numberSlice))
+
+	//doesn't work
+	//fmt.Println("array sum = ", tcm.Sum(numberArray))
+
+	fmt.Println("array slice sum = ", tcm.Sum(numberArray[:]))
+	fmt.Println("array newSum - ", newSum(numberArray))
+
+	//doesn't work
+	//fmt.Println("slice newSum - ", newSum(numberSlice))
+	fmt.Println("slice newSum - ", newSum([3]int{numberSlice[0], numberSlice[1]}))
 }
