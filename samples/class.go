deb8777e16cd03b33ef44991c94aae018c56e53e
ssburns <ssburns@gmail.com>
2015-02-23 14:44:52 -0800

    class

---
+++ samples/class.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+// TYPE
+type contact struct {
+	name     string
+	greeting string
+}
+
+// TYPES CAN CONTAIN METHODS & RETURN
+func (c contact) sayHello() string {
+	return "METHOD: " + c.name + " says hello."
+}
+
+func (c *contact) rename(newName string) {
+	c.name = newName
+}
+
+func zero(xPtr *int) {
+	*xPtr = 0
+}
+
+func main() {
+	// TYPES CAN CONTAIN DATA
+	var friend = contact{"Marcus", "Hello!"}
+	fmt.Println("DATA: " + friend.name)
+	fmt.Println("DATA: " + friend.greeting)
+
+	// TYPES CAN USE METHODS
+	fmt.Println(friend.sayHello())
+	friend.rename("Jenny")
+	fmt.Println("DATA: " + friend.name)
+	fmt.Println(friend.sayHello())
+
+
+	x := 5
+	zero(&x)
+	fmt.Println(x) // x is 0
+}
