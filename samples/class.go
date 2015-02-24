65524053f23d877eb96b75cf9cdaa45da54e2d4b
ssburns <ssburns@gmail.com>
2015-02-23 18:53:30 -0800

    ch6 p2
    Write a program that finds the smallest number in this list:
    x := []int{ 48,96,86,68, 57,82,63,70, 37,34,83,27, 19,97, 9,17, }
    */

--- samples/class.go
+++ samples/class.go
@@ -2,42 +2,8 @@ package main
 
 import "fmt"
 
-// TYPE
-type contact struct {
-	name     string
-	greeting string
-}
-
-// TYPES CAN CONTAIN METHODS & RETURN
-func (c contact) sayHello() string {
-	return "METHOD: " + c.name + " says hello."
-}
-
-func (c *contact) rename(newName string) {
-	c.name = newName
-}
-
-func zero(xPtr *int) {
-	*xPtr = 0
-}
-
 func main() {
 	// TYPES CAN CONTAIN DATA
-	var friend = contact{"Marcus", "Hello!"}
-	fmt.Println("DATA: " + friend.name)
-	fmt.Println("DATA: " + friend.greeting)
-
-	// TYPES CAN USE METHODS
-	fmt.Println(friend.sayHello())
-	friend.rename("Jenny")
-	fmt.Println("DATA: " + friend.name)
-	fmt.Println(friend.sayHello())
-
-
-	x := 5
-	zero(&x)
-	fmt.Println(x) // x is 0
-
-	fmt.Println((true && false) || (false && true) || !(false && false))
-
+	x := [6]string{"a","b","c","d","e","f"}
+	fmt.Println(x[2:5])
 }
