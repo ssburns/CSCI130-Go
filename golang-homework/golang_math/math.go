f983c62ec8f8e4c9e8926c828c88a807ba77fcff
ssburns <ssburns@gmail.com>
2015-02-24 21:14:10 -0800

    godoc

--- golang-homework/golang_math/math.go
+++ golang-homework/golang_math/math.go
@@ -8,6 +8,7 @@ func (arr byVal) Len() int {return len(arr)}
 func (arr byVal) Swap(i, j int) {arr[i], arr[j]=arr[j], arr[i]}
 func (arr byVal) Less(i, j int) bool {return arr[i] < arr[j]}
 
+// Finds the average of a series of numbers
 func Average(xs []float64) float64 {
 	total := float64(0)
 	for _, x := range xs {
