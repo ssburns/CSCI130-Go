4c5cea855c17f0c53eaa7bdb77686a65fa2f252e
ssburns <ssburns@gmail.com>
2015-02-24 21:08:17 -0800

    ch11p4_MinMax
    Create Min and Max functions which find the minimum and maximum values
    in a slice of float64s.

---
+++ golang-homework/golang_math/math.go
@@ -0,0 +1,27 @@
+package math
+
+import(
+	"sort"
+)
+type byVal []float64
+func (arr byVal) Len() int {return len(arr)}
+func (arr byVal) Swap(i, j int) {arr[i], arr[j]=arr[j], arr[i]}
+func (arr byVal) Less(i, j int) bool {return arr[i] < arr[j]}
+
+func Average(xs []float64) float64 {
+	total := float64(0)
+	for _, x := range xs {
+		total += x
+	}
+	return total / float64(len(xs))
+}
+
+func Min(xs []float64) float64 {
+	sort.Sort(byVal(xs))
+	return(xs[0])
+}
+
+func Max(xs []float64) float64 {
+	sort.Sort(byVal(xs))
+	return(xs[len(xs)-1])
+}
