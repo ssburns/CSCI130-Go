cd9fc4132682729e2530c31d1eb04228bc5c7b04
ssburns <ssburns@gmail.com>
2015-02-28 15:00:44 -0800

    ch13 done

--- golang-homework/ch13/ch13_everything.go
+++ golang-homework/ch13/ch13_everything.go
@@ -41,6 +41,9 @@ import (
 	"net/http"
 	"net/rpc"
 	"io"
+	"flag"
+	"sync"
+	"time"
 )
 
 func main() {
@@ -62,10 +65,63 @@ func main() {
 	testHashes()
 
 	//testing net
+	testNet()
+
+	//test flag
+	testFlag()
+
+	//Test sync and time
+	testSync()
+
+	//end
+	var input string
+	fmt.Scanln(&input)
+}
+
+func testSync() {
+	fmt.Println("\n\n")
+	fmt.Println("****************************************************")
+	fmt.Println("Sync, time")
+	fmt.Println("****************************************************")
+
+	m := new(sync.Mutex)
+	for i := 0; i < 10; i++ {
+		go func(i int) {
+			m.Lock()
+			fmt.Println(i, "start")
+			time.Sleep(time.Second)
+			fmt.Println(i, "end")
+			m.Unlock()
+		}(i)
+	}
+}
+
+func testFlag() {
+	fmt.Println("\n\n")
+	fmt.Println("****************************************************")
+	fmt.Println("Flag")
+	fmt.Println("****************************************************")
+
+	// Define flags
+	maxp := flag.Int("max", 6, "the max value")
+	// Parse
+	flag.Parse()
+
+	if *maxp == 6 {
+		fmt.Println("No arugments passed using default: 6")
+	}
+
+	// Generate a number between 0 and max
+	fmt.Println("Random number is:",rand.Intn(*maxp))
+}
+
+func testNet(){
 	fmt.Println("\n\n")
 	fmt.Println("****************************************************")
 	fmt.Println("net stuff active")
 	fmt.Println("****************************************************")
+	fmt.Println("Press enter to exit...")
+
 
 	go server()
 	go client()
@@ -73,10 +129,44 @@ func main() {
 	go http.HandleFunc("/hello", hello)
 	go http.ListenAndServe(":9000", nil)
 
-	//end
-	fmt.Println("Press enter to exit...")
-	var input string
-	fmt.Scanln(&input)
+	go server2()
+	go client2()
+}
+
+type Server struct {}
+func (this *Server) Negate(i int64, reply *int64) error {
+	*reply = -i
+	return nil
+}
+
+func server2() {
+	rpc.Register(new(Server))
+	ln, err := net.Listen("tcp", ":9998")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for {
+		c, err := ln.Accept()
+		if err != nil {
+			continue
+		}
+		go rpc.ServeConn(c)
+	}
+}
+func client2() {
+	c, err := rpc.Dial("tcp", "127.0.0.1:9998")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	var result int64
+	err = c.Call("Server.Negate", int64(999), &result)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Server.Negate(999) =", result)
+	}
 }
 
 func hello(res http.ResponseWriter, req *http.Request) {
@@ -92,7 +182,7 @@ func hello(res http.ResponseWriter, req *http.Request) {
 		<title>Hello World</title>
 	</head>
 	<body>
-		Hello World!
+		Hello Go World!
 	</body>
 </html>`,
 	)
