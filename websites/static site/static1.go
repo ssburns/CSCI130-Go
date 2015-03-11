57712b07bf9644c2c10ca367228910e3b705408b
ssburns <ssburns@gmail.com>
2015-03-10 22:45:56 -0700

    static site

--- websites/static site/static1.go
+++ websites/static site/static1.go
@@ -1,19 +1,13 @@
-package hello
+package static
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func init() {
 	http.HandleFunc("/", handler)
-	http.HandleFunc("/sign", sign)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, world!")
-}
-
-func sign(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "No Autographs!")
+	http.ServeFile(w,r, "public/"+r.URL.Path)
 }
