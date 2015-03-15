9f851a081917c4381df811a0680e1e27aabb44ad
ssburns <ssburns@gmail.com>
2015-03-14 23:09:15 -0700

    text template

--- misc/text/template/main.go
+++ misc/text/template/main.go
@@ -11,25 +11,44 @@ type Recipient struct{
 	Donated bool
 }
 
+type UpcomingEvents struct{
+	Events []string
+}
+
 func main() {
-	const letter =`Dear {{.Honorific}} {{.Name}}
-{{if .Donated}}
+	const letter =`Dear {{.Person.Honorific}} {{.Person.Name}}
+{{if .Person.Donated}}
 Thank you for donating!
 {{else}}
 It makes us sad that you didn't donate, something something puppies and kittens. :(
 {{end}}
 `
+	const letter2 = "{{.Extra}}"
 
 	recipients := []Recipient{
 		{"Mr", "Burns", false},
 		{"Mr", "Smithers", true},
 		{"Mrs", "Simpson", true},
+		{"Mrs", "Krabappel", false},
 	}
+
+	upcomingEvents := UpcomingEvents{Events:[]string{
+		"event1",
+		"event2",
+		"event3"}}
+
 
 	t := template.Must(template.New("letter").Parse(letter))
 
 	for _,r := range recipients{
-		err := t.Execute(os.Stdout, r)
+		data := struct{
+			Person *Recipient
+			MoreInfo UpcomingEvents
+			}{
+			&r,
+			upcomingEvents,
+			}
+		err := t.Execute(os.Stdout, data)
 		if err != nil{
 			log.Println("executing template:", err)
 		}
