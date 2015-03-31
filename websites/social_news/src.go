ec4da384f55eaa5cd70cdf50b38e4ec825e148ae
ssburns <ssburns@gmail.com>
2015-03-30 20:29:47 -0700

    Initial Commit

---
+++ websites/social_news/src.go
@@ -0,0 +1,17 @@
+package hello
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func init() {
+//	http.HandleFunc("/", handler)
+
+}
+
+
+//
+//func handler(w http.ResponseWriter, r *http.Request) {
+//	fmt.Fprint(w, "Hello, world!")
+//}
