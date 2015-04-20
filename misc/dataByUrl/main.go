53563c4fa534ba267b985cd2648109ea0889b70e
ssburns <ssburns@gmail.com>
2015-04-19 20:41:56 -0700

    data by Url

--- misc/dataByUrl/main.go
+++ misc/dataByUrl/main.go
@@ -7,17 +7,20 @@ import (
 )
 
 var (
-	tmpl = template.Must(template.ParseFiles("root.html"))
+	tmpl = template.Must(template.ParseFiles("root.html", "view.html"))
 )
 
 
 func init() {
 	http.HandleFunc("/", rootHandler)
-	http.HandleFunc("/")
+	http.HandleFunc("/view", viewHandler)
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl.ExecuteTemplate(w, "root.html", nil)
 }
 
-func
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	pageData := struct{Data1, Data2 string}{r.FormValue("name"), r.FormValue("link")}
+	tmpl.ExecuteTemplate(w,"view.html", pageData)
+}
